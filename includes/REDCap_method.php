0e14571879c4f9070d628d89c3a2fa057d6981da
Rollie Parrish <rollie.parrish@ampa.org>
2013-09-02 15:44:37 -0700

    added comment explaining why this doesn't work yet.

--- includes/REDCap_method.php
+++ includes/REDCap_method.php
@@ -1,7 +1,9 @@
 <?php
 
+// This does not function correctly yet. Need to be able to extract
+// record labels using the REDCap::getData method. 
 
-        $method_data = REDCap::getData('json', $_GET['record']);
-        $json_decoded = json_decode($method_data, TRUE);
-        $data = $json_decoded[0];
+$method_data = REDCap::getData('json', $_GET['record']);
+$json_decoded = json_decode($method_data, TRUE);
+$data = $json_decoded[0];
 ?>
