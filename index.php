85a3f0fb082faf4b88cbf3dc5fef4f5f5d7275eb
Rollie Parrish <rollie.parrish@ampa.org>
2013-09-02 15:42:39 -0700

    cleaned up

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 //// load the required classes and configuration options 
 require_once ('includes/common.php');
 
-
 //// load the config file for this project
 $config = parse_ini_file("pid/" . $project_id . "/config.ini");
 
@@ -28,6 +27,7 @@ exit();
 }
 
 
+//// Using REDCap API
 require_once ("includes/REDCap_API.php");
 
 //// Using REDCap::getData method */
@@ -40,12 +40,15 @@ require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 
 include_once('includes/tbs_class.php');
 //include_once('includes/tbs_plugin_opentbs.php');
+// future use - for templates in Open Document format (ie. .docx, .ppt, etc.)
+
 
 $TBS = new clsTinyButStrong;
 //$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN);
+// future use - for templates in Open Document format
+
 
 $TBS->LoadTemplate(dirname(__FILE__) . '/pid/'.$project_id.'/'.$_GET['template']);
 $TBS->Show();
-
 ?>
 
