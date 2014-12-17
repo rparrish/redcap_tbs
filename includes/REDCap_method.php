4a92d9cd2c334c07e996ea0b501e3bcfe4eae9c1
Rollie Parrish <rollie.parrish@ampa.org>
2014-12-16 19:33:13 -0800

    switched back to allowing any project id

--- includes/REDCap_method.php
+++ includes/REDCap_method.php
@@ -1,7 +1,7 @@
 <?php
 
-//$method_data = REDCap::getData($_GET['pid'], 'json', $_GET['record']);
-$method_data =REDCap::getData('37', 'json', '2');
+$method_data = REDCap::getData($_GET['pid'], 'json', $_GET['record']);
+//$method_data =REDCap::getData('137', 'json', '2');
 
 $json_decoded = json_decode($method_data, TRUE);
 $data = $json_decoded[0];
