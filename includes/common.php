8c49ce5e71c3c795a5763c69b54d544da81d4c0f
Rollie Parrish <rollie.parrish@ampa.org>
2014-12-20 14:11:25 -0800

    removed hard requirement for version 6.2.2

--- includes/common.php
+++ includes/common.php
@@ -1,16 +1,13 @@
 <?php
 
-
-require_once  '../../redcap_v6.2.2/Config/init_project.php';
-
-
-//Required files
-require_once APP_PATH_DOCROOT . '/ProjectGeneral/form_renderer_functions.php';
-
-
 // Call the REDCap Connect file in the main "redcap" directory
 require_once "../../redcap_connect.php";
 
+//require_once  '../../redcap_v6.2.2/Config/init_project.php';
+require_once  APP_PATH_DOCROOT . '/Config/init_project.php';
+
+//Required files
+require_once APP_PATH_DOCROOT . '/ProjectGeneral/form_renderer_functions.php';
 
 //// confirm REDCap credentials via cURL
 function CheckAccess() {
