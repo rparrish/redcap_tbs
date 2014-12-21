63b986fba0935a6c3d9032653feb8158914eff53
Rollie Parrish <rollie.parrish@ampa.org>
2014-12-21 12:44:03 -0800

    fixes #8

--- includes/REDCap_method.php
+++ includes/REDCap_method.php
@@ -1,6 +1,7 @@
 <?php
 
-$method_data = REDCap::getData($_GET['pid'], 'json', $_GET['record']);
+$method_data = REDCap::getData($_GET['pid'], 'json', $_GET['record'], NULL, NULL, NULL, FALSE, FALSE, FALSE, FALSE, TRUE);
+
 //$method_data =REDCap::getData('137', 'json', '2');
 
 $json_decoded = json_decode($method_data, TRUE);
